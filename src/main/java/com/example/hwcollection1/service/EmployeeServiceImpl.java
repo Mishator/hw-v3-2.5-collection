c615f834f1ae147600577e218d893b5296168adf
Mishator <voyteh89@mail.ru>
2023-07-04 02:38:55 +0300

    Реализовали класс EmployeeServiceImpl

--- src/main/java/com/example/hwcollection1/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/hwcollection1/service/EmployeeServiceImpl.java
@@ -1,20 +1,42 @@
 package com.example.hwcollection1.service;
 
 import com.example.hwcollection1.model.Employee;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private final List<Employee> employeeList;
+
+    public EmployeeServiceImpl() {
+        this.employeeList = new ArrayList<>();
+    }
+
     @Override
     public Employee add(String firstName, String lastName) {
-        return null;
+        Employee employee = new Employee(firstName, lastName);
+        employeeList.add(employee);
+        return employee;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
+            employeeList.remove(employee);
+        }
         return null;
     }
 
     @Override
     public Employee find(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
+            return employee;
+        }
+
         return null;
     }
 }
